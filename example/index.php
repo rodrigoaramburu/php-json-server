7d638f4906d53b31ae83d54940387878b843f2bf
Rodrigo Aramburu <rodrigoaramburu@gmail.com>
2022-08-05 16:59:21 -0300

    feat: adicionado middleware de rotas estaticas lendo rotas de array e arquivo

--- example/index.php
+++ example/index.php
@@ -1,11 +1,15 @@
 <?php
 
+use JsonServer\Middlewares\StaticMiddleware;
 use JsonServer\Server;
 
 require __DIR__.'/../vendor/autoload.php';
 
 $server = new Server(__DIR__.'/db.json');
 
+$staticMiddeware = new StaticMiddleware(__DIR__.'/static.json');
+
+$server->addMidleware($staticMiddeware);
 $path = $_SERVER['REQUEST_URI'];
 $data = file_get_contents('php://input');
 $response = $server->handle($_SERVER['REQUEST_METHOD'], $path, $data);
---
+++ src/Middlewares/StaticMiddleware.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JsonServer\Middlewares;
+
+use Exception;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class StaticMiddleware extends Middleware
+{
+    private array $routes = [];
+
+    public function __construct(string|array $routes = 'static.json')
+    {
+        if (is_string($routes)) {
+            if (! file_exists($routes)) {
+                throw new Exception('cannot open file '.$routes);
+            }
+            $r = json_decode(file_get_contents($routes), true);
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                throw new Exception('cannot read file '.$routes);
+            }
+            $this->routes = $r;
+        }
+        if (is_array($routes)) {
+            $this->routes = $routes;
+        }
+    }
+
+    public function process(RequestInterface $request, Handler $handler): ResponseInterface
+    {
+        $route = $this->getRoute($request);
+        if ($route !== null) {
+            return $this->response($route);
+        }
+
+        return $handler->handle($request);
+    }
+
+    private function getRoute(RequestInterface $request): ?array
+    {
+        if (! array_key_exists($request->getUri()->getPath(), $this->routes)) {
+            return null;
+        }
+        $route = $this->routes[$request->getUri()->getPath()];
+
+        if (array_key_exists($request->getMethod(), $route)) {
+            return $route[$request->getMethod()];
+        }
+
+        return null;
+    }
+
+    private function response(array $route): ResponseInterface
+    {
+        $psr17Factory = new Psr17Factory();
+
+        $response = $psr17Factory->createResponse($route['statusCode'] ?? 200)
+                        ->withBody($psr17Factory->createStream($route['body'] ?? ''));
+
+        foreach ($route['headers'] ?? [] as $header => $value) {
+            $response = $response->withHeader($header, $value);
+        }
+
+        return $response;
+    }
+}
