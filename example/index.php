03c32ebea4f9e6e3b611cce855db7058e5efd349
Rodrigo Aramburu <rodrigoaramburu@gmail.com>
2022-08-06 10:45:14 -0300

    fix: adicionado headers ao request

--- example/index.php
+++ example/index.php
@@ -8,10 +8,11 @@ require __DIR__.'/../vendor/autoload.php';
 $server = new Server(__DIR__.'/db.json');
 
 $staticMiddeware = new StaticMiddleware(__DIR__.'/static.json');
+$server->addMiddleware($staticMiddeware);
 
-$server->addMidleware($staticMiddeware);
 $path = $_SERVER['REQUEST_URI'];
-$data = file_get_contents('php://input');
-$response = $server->handle($_SERVER['REQUEST_METHOD'], $path, $data);
+$body = file_get_contents('php://input');
+$headers = getallheaders();
+$response = $server->handle($_SERVER['REQUEST_METHOD'], $path, $body, $headers);
 
 $server->send($response);
